4f119284dd02c4debcfc2207348f7a87244b46ff
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-13 19:47:43 +1300

    add updated comments from the db with polling

--- src/components/commentbox.js
+++ src/components/commentbox.js
@@ -1,17 +1,36 @@
 var React = require('react')
 var CommentForm = require('./commentform.js')
 var CommentList = require('./commentlist.js')
+var ajax = require('../ajax.js')
 
 
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    console.log("get initial state", this.props.url, this.props.data)
+    return {data: []}
+
+  },
+  componentDidMount: function() {
+
+    console.log("componentdidmount", this.props.url, this.state.data)
+
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+ },
+ loadCommentsFromServer: function(){
+   ajax(this.props.url, function(data) {
+         this.setState({data: data})
+       }
+       .bind(this))
+ },
+
   render: function(){
-    console.log(this.props.data)
+    console.log("in render", this.state.data)
     return (
       <div className="commentBox">
         <h1>CommentBox </h1>
         <CommentForm />
-        <CommentList data={this.props.data}/>
+        <CommentList data={this.state.data}/>
       </div>
     )
   }
