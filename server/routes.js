b976b040e113a77aad56c23f41624305ca978a7b
gaynor-landmark <g.l.landmark@gmail.com>
2016-03-13 17:11:25 +1300

    add GET to comments in the db

--- server/routes.js
+++ server/routes.js
@@ -1,6 +1,22 @@
 'use strict'
 
-exports = module.exports = function (app) {
 
+var knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+      filename: './data/comments.sqlite'
+    },
+    useNullAsDefault: true
+  })
+
+exports = module.exports = function (app) {
+    app.get('/comments', function (req, res) {
+        knex('comments')
+        .select('*')
+        .then(function(data){
+          console.log("in app get", data)
+          res.send(data)
+        })
+    })
 
 }
